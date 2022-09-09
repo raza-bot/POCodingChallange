494e32261b518ed4450fe6a372a92344c3fd0b84
raza <raza.ghulam2021@gmail.com>
2022-09-08 21:14:32 -0700

    Max sum sub array code added

--- CodingChallenge/src/Week2/Challenge.java
+++ CodingChallenge/src/Week2/Challenge.java
@@ -17,6 +17,27 @@ public class Challenge {
 		
 		//Second method can also be applied. Find the max and min 
 		// in the array and subtract them. 
+		
+		//max sum of sub-array
+//		int arr1[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4}; 
+		int arr1[] = {2,2,-2}; 
+		
+		int sum = 0; 
+		int max = 0; 
+		
+		for (int i = 0; i < arr1.length; i++) {
+			if (arr1[i] > max + arr1[i] ) {
+				max = arr1[i];  
+			}else {
+				max = max + arr1[i];  
+			}
+			
+			if (max > sum) {
+				sum = max; 
+			}
+		}
+		
+		System.out.println(sum);
 	}
 
 }
